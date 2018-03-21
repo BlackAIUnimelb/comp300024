252ec35514d7be327ccc23b7d22ed36421f22fd4
linus <penxumelb@gmail.com>
2018-03-21 18:16:25 +1100

    Added formatInput func

--- part-A/main.py
+++ part-A/main.py
@@ -1,6 +1,23 @@
-def main():
-    pass
 
+class BoardAnalyser():
+
+    def __init__(self):
+        self.board = None;
+        self.goal = None;
+
+    def formatInput(self):
+
+        self.board = [];
+        # Read board
+        for i in range(0, 8):
+            content = input();
+            self.board.append(content.split());
+
+        self.goal = input();
+
+        print(self.board, self.goal);
 
 if __name__ == '__main__':
-    main();
+
+    ba = BoardAnalyser();
+    ba.formatInput();
