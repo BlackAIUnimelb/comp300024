11ef2f20216a0468f7d6d053bfa010a88a01d58c
linus <penxumelb@gmail.com>
2018-03-21 18:54:57 +1100

    added getPosOfChar()

--- part-A/main.py
+++ part-A/main.py
@@ -4,6 +4,7 @@ class BoardAnalyser():
     def __init__(self):
         self.board = None;
         self.goal = None;
+        self.chars = ['-', 'X', 'O', '@'];
 
     def formatInput(self):
 
@@ -15,9 +16,51 @@ class BoardAnalyser():
 
         self.goal = input();
 
-        print(self.board, self.goal);
+        return True;
+
+    # Returns all coordinates of CHAR
+    def getPosOfChar(self, char, boardSize = 8):
+
+        resultPos = [];
+        # 4 types of char: - X O @
+        if char not in self.chars:
+            return None;
+
+        for x in range(0, boardSize):
+
+            for y in range(0, boardSize):
+
+                if char == self.board[x][y]:
+                    # (col, row)
+                    resultPos.append((y,x));
+
+        return resultPos;
+
+    def countDirectMoves(self, dots):
+
+        if (len(dots) == 0):
+            return 0;
+
+        for dot in dots:
+
+            col = dot[0];
+            row = dot[1];
+
+            # col + 1
+            
+
+            # col - 1
+
+            # row + 1
+
+            # row - 1
+
 
 if __name__ == '__main__':
 
     ba = BoardAnalyser();
     ba.formatInput();
+    # blackDots = ba.getPosOfChar('@');
+    whiteDots = ba.getPosOfChar('O')
+
+    ba.countDirectMoves(whiteDots);
