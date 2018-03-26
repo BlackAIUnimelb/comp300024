72bfbfb5222a705678c7ede55cb2fc6992e826b3
chaoxinw <wcx970303@163.com>
2018-03-27 10:55:23 +1100

    add A star search, return shortest path if given two positions

--- part-A/main.py
+++ part-A/main.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+import copy
 
 class BoardAnalyser():
 
@@ -133,8 +134,126 @@ class BoardAnalyser():
         return sumdist;
 
 
+
+class State(object):
+    def __init__(self, value, parent, start = 0, goal = 0):
+        self.children = []
+        self.parent = parent
+        self.value = value
+        self.dist = 0
+        if parent:
+            self.path = parent.path[:]
+            self.path.append(value)
+            self.start = parent.start
+            self.goal = parent.goal
+        else:
+            self.path = [value]
+            self.start = start
+            self.goal = goal
+
+        def GetDist(self):
+            pass
+        def CreateChildren(self):
+            pass
+
+class StatePieces(State):
+    def __init__(self, value, parent, environ, start = 0, goal = 0):
+        super(StatePieces, self).__init__(value, parent, start, goal)
+        #super().__init__()
+        self.environ = copy.deepcopy(environ)
+        #self.path_cost = path_cost
+        self.dist = self.GetDist()
+
+    def GetDist(self):
+        #priority
+        if (self.value[0] == self.goal[0] and self.value[1] == self.goal[1]):   
+            return 0
+        dist = abs(self.value[0] - self.goal[0]) + abs(self.value[1] - self.goal[1])
+        return dist
+
+    def CreateChildren(self):
+    	#create nodes
+        if not self.children:
+
+            direction = [
+                (0, -1), (0, 1), (-1, 0), (1, 0)    #上下左右??
+            ]
+            for i in range(0, 4):
+            	#下一个点 考虑直接走的情况
+                newCol = self.value[0] + direction[i][0]
+                newRow = self.value[1] + direction[i][1]
+                if (newCol > 7 or newCol < 0 or newRow > 7 or newRow < 0):
+                    continue
+                val = (newCol, newRow)
+
+                theChar = self.environ[newCol][newRow]
+                if theChar == '-':
+                    newEnviron = copy.deepcopy(self.environ)
+                    newEnviron[self.value[0]][self.value[1]] = '-'
+                    newEnviron[val[0]][val[1]] = 'O'
+                    child = StatePieces(val, self, newEnviron, 0, 0)
+                    self.children.append(child)
+                    continue
+
+                # 再下一个点   考虑跳的情况
+                newNextCol = newCol + direction[i][0]
+                newNextRow = newRow + direction[i][1]
+                if ((theChar in ['O','@']) and (newNextCol > 7 or newNextCol < 0 or newNextRow > 7 or newNextRow < 0)):
+                    continue
+                theNextChar = self.environ[newNextCol][newNextRow]
+                if ((theChar in ['O','@']) and theNextChar in ['O','@']):
+                    continue
+                if ((theChar in ['O','@']) and theNextChar == '-'):
+                    val = (newNextCol, newNextRow)
+
+                    newEnviron = copy.deepcopy(self.environ)
+                    newEnviron[self.value[0]][self.value[1]] = '-'
+                    newEnviron[val[0]][val[1]] = 'O'
+                    child = StatePieces(val, self, newEnviron, 0, 0)
+                    self.children.append(child)
+
+
+class AStar_Solver:
+    def __init__(self, start, goal, environ):
+        self.path = []
+        self.visitedQueue = []
+        self.priorityQueue = PriorityQueue()
+        self.start = start
+        self.goal = goal
+        self.environ = environ
+
+    def Solve(self):
+        startState = StatePieces(self.start, 0, self.environ, self.start, self.goal)
+        count = 0
+        self.priorityQueue.put((0, count, startState))
+
+        while(not self.path and self.priorityQueue.qsize()):
+        	#expand most desirable node
+            closestChild = self.priorityQueue.get()[2]
+            closestChild.CreateChildren()
+
+            self.visitedQueue.append(closestChild.value)
+
+            for child in closestChild.children:
+                if child.value not in self.visitedQueue:
+                    count +=1
+                    if not child.dist:
+                        self.path = child.path
+                        break
+                    self.priorityQueue.put((child.dist, count, child))
+        if not self.path:
+            print("Goal of " + self.goal + " is not possible")
+        return self.path
+
 if __name__ == '__main__':
 
     ba = BoardAnalyser();
     ba.formatInput();
     ba.printWBMoves();
+
+    start = (2, 6)       #a white piece position
+    goal = (3, 3)        #一个指定黑棋 周围的某个点
+    a = AStar_Solver(start, goal, copy.deepcopy(ba.board))
+    a.Solve()
+    for i in range(len(a.path)):
+        print(a.path[i])
